bec3450fcdf1275ff14d5d51d5ec903484a12714
Alcoaster <dae29@tpu.ru>
2026-02-07 20:26:32 +0700

    упрощение и исправление дешифратора

--- app/src/logic/cypher.js
+++ app/src/logic/cypher.js
@@ -2,17 +2,10 @@ import {RusABC} from './abc.js'
 export class CypherMachine {
     caesarChar(char,keyword){
         const abc = new RusABC();
-        const char_key = abc.getKey(char)
-        const new_char_key = (char_key + abc.getKey(keyword.slice(0,1)))%32+1
-        const new_char = abc.getSymbol(new_char_key)
-        return new_char
+        return abc.summarizeSymbols(char,keyword.slice(0,1))
     }
     caesarCharDecode(char,keyword){
         const abc = new RusABC();
-        const char_key = abc.getKey(char)
-        const new_char_key = char_key - abc.getKey(keyword.slice(0,1))+1
-        const new_char = abc.getSymbol(Math.abs(new_char_key % 32))
-
-        return new_char
+        return abc.subtractSymbols(char,keyword.slice(0,1))
     }
 }
