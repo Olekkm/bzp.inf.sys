46d518bc0da461f6cfe1593788cfcaf67827b725
Olekkm <138139365+Olekkm@users.noreply.github.com>
2026-02-11 13:47:29 +0700

    Added modified S-Block funcrion in ViginereCoder. Constructor was updated

--- app/src/logic/vigenere.js
+++ app/src/logic/vigenere.js
@@ -4,19 +4,45 @@ class VigenereCoder {
     abc
     keyword
 
-    //gets keyword and abc as arguments
+    Encode
+    Decode
+
+    //gets type, keyword and abc as arguments
     //has default values, so arguments can be skipped
-    constructor(keyword, abc = new RusABC()) {
-        if (keyword) {
+    //
+    //types: "BaseVigenere" (text: string any length, keyword: string any lenght)
+    //       "S_Block" (text: string length = 4, keyword: string lenght = 16)
+    //       "Merge_Block" (text: string length = 4, keyword: string lenght = 16)
+    //       "S_Block_Mod" (text: string length = 4, keyword: string lenght = 16)
+    //        default: "BaseViginere"
+    constructor(type, keyword, abc = new RusABC()) {
+        if (keyword != undefined) {
             this.keyword = keyword
         }
-        this.keyword = "_"
+        else { this.keyword = "_" }
+
         this.abc = abc
+
+        const encoder = {
+            "BaseVigenere": [this.BaseViginereEncode, this.BaseVigenereDecode],
+            "S_Block": [this.EncodeBlock, this.DecodeBlock],
+            "Merge_Block": [(textBlock, keyword) => this.mergeBlock(textBlock, keyword, true),
+            (textBlock, keyword) => this.mergeBlock(textBlock, keyword, false)],
+            "S_Block_Mod": [this.EncodeBlockMod, this.DecodeBlockMod]
+        }
+
+        if (type == undefined) {
+            [this.Encode, this.Decode] = encoder["BaseVigenere"]
+        }
+        else {
+            if (encoder[type] == undefined) { throw new Error("incorrect encoder type") }
+            [this.Encode, this.Decode] = encoder[type]
+        }
     }
 
     //gets text and keyword
     //returns cyphered by Vigenere's method string
-    Encode(plainText, keyword) {
+    BaseViginereEncode(plainText, keyword) {
         const _keyword = keyword ? keyword : this.keyword
 
         if (_keyword.length < 1) { throw new RangeError("") }
@@ -38,52 +64,7 @@ class VigenereCoder {
 
     //gets cyphered text and keyword
     //returns decoded by Vigenere's method string
-    Decode(encodedText, keyword) {
-        const _keyword = keyword ? keyword : this.keyword
-
-        if (_keyword.length < 1) { throw new RangeError("") }
-
-        for (const i of _keyword) {
-            this.abc.checkSymbolInAbc(i)
-        }
-
-        let baseKey = "_"
-        let decodedText = ""
-        for (let i = 0; i < encodedText.length; i++) {
-            this.abc.checkSymbolInAbc(encodedText[i])
-
-            baseKey = this.abc.summarizeSymbols(baseKey, _keyword[i % _keyword.length])
-            decodedText = decodedText + this.abc.subtractSymbols(encodedText[i], baseKey)
-        }
-        return decodedText
-    }
-
-    //gets text and keyword
-    //returns cyphered by Vigenere's method string
-    baseVigenereEncode(plainText, keyword) {
-        const _keyword = keyword ? keyword : this.keyword
-
-        if (_keyword.length < 1) { throw new RangeError("") }
-
-        for (const i of _keyword) {
-            this.abc.checkSymbolInAbc(i)
-        }
-
-        let baseKey = "_"
-        let encodedText = ""
-        for (let i = 0; i < plainText.length; i++) {
-            this.abc.checkSymbolInAbc(plainText[i])
-
-            baseKey = this.abc.summarizeSymbols(baseKey, _keyword[i % _keyword.length])
-            encodedText = encodedText + this.abc.summarizeSymbols(baseKey, plainText[i])
-        }
-        return encodedText
-    }
-
-
-    //gets cyphered text and keyword
-    //returns decoded by Vigenere's method string
-    baseVigenereDecode(encodedText, keyword) {
+    BaseVigenereDecode(encodedText, keyword) {
         const _keyword = keyword ? keyword : this.keyword
 
         if (_keyword.length < 1) { throw new RangeError("") }
@@ -124,63 +105,79 @@ class VigenereCoder {
         return keyTemp
     }
 
-    //basic block encoder
+    //basic S-block encoder
     //gets textBlock (string of 4 symbols) and key (string of 16 symbols)
     //returns encoded textBlock
     EncodeBlock(textBlock, keyword) {
         if (!(textBlock.length == 4 && keyword.length == 16)) { throw new Error("wrong length args") }
 
-        return this.Encode(textBlock, this.#calcBlockKeyword(keyword))
+        return this.BaseViginereEncode(textBlock, this.#calcBlockKeyword(keyword))
     }
 
-    //basic block decoder
+    //basic S-block decoder
     //gets encoded textBlock (string of 4 symbols) and key (string of 16 symbols)
     //returns decoded textBlock
     DecodeBlock(textBlock, keyword) {
         if (!(textBlock.length == 4 && keyword.length == 16)) { throw new Error("wrong length args") }
 
-        return this.Decode(textBlock, this.#calcBlockKeyword(keyword))
+        return this.BaseVigenereDecode(textBlock, this.#calcBlockKeyword(keyword))
     }
 
 
     //placeholder for encoding with block merge (frw_merge_block в презентации)
     mergeBlock(blockIn, keyIn, encode) {
-         if (blockIn.length != 4 || keyIn.length != 16 || typeof blockIn !== 'string' || typeof keyIn!== 'string'){
+        if (blockIn.length != 4 || keyIn.length != 16 || typeof blockIn !== 'string' || typeof keyIn !== 'string') {
             return "input-error"
-         }
-         let M = [0,1,2,3]
-         let sum = 0
-         const array = this.abc.getKeyArray(keyIn)
-         for(let i = 0; i < 16; i++){
-            let a = (24 + sum + (-1)**i * array[i]) % 24
-            sum = (24 + sum + (-1)**i * array[i]) % 24
-            
-         }
-         for(let k = 0; k < 3; k++){
+        }
+        let M = [0, 1, 2, 3]
+        let sum = 0
+        const array = this.abc.getKeyArray(keyIn)
+        for (let i = 0; i < 16; i++) {
+            let a = (24 + sum + (-1) ** i * array[i]) % 24
+            sum = (24 + sum + (-1) ** i * array[i]) % 24
+
+        }
+        for (let k = 0; k < 3; k++) {
             let t = sum % (4 - k)
-            sum = (sum - t)/(4 - k)
+            sum = (sum - t) / (4 - k)
             let tmp = M[k]
             M[k] = M[k + t]
             M[k + t] = tmp
-         }
-         var inp = this.abc.getKeyArray(blockIn)
-         if (encode == true){
-         for(let j = 0; j < 4; j++){
-            let b = M[(1+j) % 4]
-            let a = M[j % 4]
-            inp[b] = (inp[b] + inp[a]) % 32
+        }
+        var inp = this.abc.getKeyArray(blockIn)
+        if (encode == true) {
+            for (let j = 0; j < 4; j++) {
+                let b = M[(1 + j) % 4]
+                let a = M[j % 4]
+                inp[b] = (inp[b] + inp[a]) % 32
             }
-         }
-         if (encode == false)
-         {
-            for (let j=3; j>-1;j--)
-            {
-                let b = M[(1+j) % 4]
+        }
+        if (encode == false) {
+            for (let j = 3; j > -1; j--) {
+                let b = M[(1 + j) % 4]
                 let a = M[j % 4]
                 inp[b] = (32 + inp[b] - inp[a]) % 32
             }
-         }
-         return this.abc.getTextFromKeys(inp) 
+        }
+        return this.abc.getTextFromKeys(inp)
+    }
+
+    //Modified S-block encoder
+    //gets textBlock (string of 4 symbols) and key (string of 16 symbols)
+    //returns encoded textBlock
+    EncodeBlockMod(textBlock, keyword) {
+        let temp = this.mergeBlock(textBlock, keyword, true)
+        temp = this.EncodeBlock(temp, keyword)
+        return this.mergeBlock(temp, keyword, true)
+    }
+
+    //Modified S-block decoder
+    //gets encoded textBlock (string of 4 symbols) and key (string of 16 symbols)
+    //returns decoded textBlock
+    DecodeBlockMod(textBlock, keyword) {
+        let temp = this.mergeBlock(textBlock, keyword, false)
+        temp = this.DecodeBlock(temp, keyword)
+        return this.mergeBlock(temp, keyword, false)
     }
 }
 
@@ -189,6 +186,8 @@ class VigenereCoder {
 export function consoleCheck() {
     const word = prompt("plainText")
     const key = prompt("key")
-    const worker = new VigenereCoder()
-    alert(worker.DecodeBlock(word, key))
+    const worker = new VigenereCoder("S_Block_Mod")
+    const coded = worker.Encode(word, key)
+    alert(coded)
+    alert(worker.Decode(coded, key))
 }
